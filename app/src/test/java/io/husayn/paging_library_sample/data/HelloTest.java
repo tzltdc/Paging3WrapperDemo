a2d0ee02680c6a9471fa995c43b9752cdf315c7d
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-11-11 13:17:07 -0800

    Minimum changes to make unit test runnable.

---
+++ app/src/test/java/io/husayn/paging_library_sample/data/HelloTest.java
@@ -0,0 +1,20 @@
+package io.husayn.paging_library_sample.data;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+/**
+ * Created by husaynhakeem on 9/26/17.
+ */
+public class HelloTest {
+
+
+    @Test
+    public void returnCorrectSize_whenDatabaseIsFull() {
+
+    }
+
+}
