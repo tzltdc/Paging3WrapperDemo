2c7c880535c7d7260f591113ef77fe834fd6dcdf
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-11-11 14:41:20 -0800

    Migrate the PagedList and PagedListAdapter but still use the older DataSource API.

--- app/src/main/java/io/husayn/paging_library_sample/listing/MainViewModel.java
+++ app/src/main/java/io/husayn/paging_library_sample/listing/MainViewModel.java
@@ -1,12 +1,16 @@
 package io.husayn.paging_library_sample.listing;
 
+import androidx.annotation.Nullable;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Transformations;
 import androidx.lifecycle.ViewModel;
-import androidx.paging.LivePagedListBuilder;
-import androidx.paging.PagedList;
+import androidx.paging.Pager;
+import androidx.paging.PagingConfig;
+import androidx.paging.PagingData;
+import androidx.paging.PagingLiveData;
 import androidx.paging.PagingSource;
+import androidx.paging.RemoteMediator;
 import io.husayn.paging_library_sample.PokemonApplication;
 import io.husayn.paging_library_sample.data.Pokemon;
 import io.husayn.paging_library_sample.data.PokemonDao;
@@ -17,29 +21,31 @@ public class MainViewModel extends ViewModel {
   private static final int INITIAL_LOAD_KEY = 0;
   private static final int PAGE_SIZE = 20;
   private static final int PREFETCH_DISTANCE = 5;
-  final LiveData<PagedList<Pokemon>> pokemonList;
+  final LiveData<PagingData<Pokemon>> pokemonList;
   private final MutableLiveData<Boolean> query = new MutableLiveData<>();
 
   public MainViewModel() {
     PokemonDao pokemonDao =
         PokemonDataBase.getInstance(PokemonApplication.getContext()).pokemonDao();
-    PagedList.Config config =
-        new PagedList.Config.Builder()
-            .setPageSize(PAGE_SIZE)
-            .setPrefetchDistance(PREFETCH_DISTANCE)
-            .setEnablePlaceholders(true)
-            .build();
-
     pokemonList =
-        Transformations.switchMap(query, orderByDesc -> liveData(pokemonDao, config, orderByDesc));
+        Transformations.switchMap(
+            query, orderByDesc -> getPagedListLiveData(pokemonDao, orderByDesc));
   }
 
-  private LiveData<PagedList<Pokemon>> liveData(
-      PokemonDao pokemonDao, PagedList.Config config, Boolean orderByDesc) {
+  private LiveData<PagingData<Pokemon>> getPagedListLiveData(
+      PokemonDao pokemonDao, Boolean orderByDesc) {
+    return PagingLiveData.getLiveData(liveData(pokemonDao, orderByDesc));
+  }
 
-    return new LivePagedListBuilder<>(() -> factory(pokemonDao, orderByDesc), config)
-        .setInitialLoadKey(INITIAL_LOAD_KEY)
-        .build();
+  private Pager<Integer, Pokemon> liveData(PokemonDao pokemonDao, Boolean orderByDesc) {
+    PagingConfig pagingConfig = new PagingConfig(PAGE_SIZE, PREFETCH_DISTANCE, true);
+    return new Pager<>(
+        pagingConfig, INITIAL_LOAD_KEY, remoteMediator(), () -> factory(pokemonDao, orderByDesc));
+  }
+
+  @Nullable
+  private RemoteMediator<Integer, Pokemon> remoteMediator() {
+    return null;
   }
 
   private PagingSource<Integer, Pokemon> factory(PokemonDao pokemonDao, Boolean orderByDesc) {
