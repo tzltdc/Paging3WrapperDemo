a652e69232ecf171ac319d63df1c945de45193e0
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-11-11 23:23:53 -0800

    Timber.i("endOfPaging:%s, requested:%s, responded:%s", endOfPaging, requested, responded);

--- app/src/main/java/io/husayn/paging_library_sample/listing/EndOfPagingMapper.java
+++ app/src/main/java/io/husayn/paging_library_sample/listing/EndOfPagingMapper.java
@@ -1,8 +1,14 @@
 package io.husayn.paging_library_sample.listing;
 
+import timber.log.Timber;
+
 class EndOfPagingMapper {
 
   public static boolean endOfPaging(PagingAction action) {
-    return action.request().queryConfig().countPerPage() > action.response().list().size();
+    int requested = action.request().queryConfig().countPerPage();
+    int responded = action.response().list().size();
+    boolean endOfPaging = requested > responded;
+    Timber.i("endOfPaging:%s, requested:%s, responded:%s", endOfPaging, requested, responded);
+    return endOfPaging;
   }
 }
