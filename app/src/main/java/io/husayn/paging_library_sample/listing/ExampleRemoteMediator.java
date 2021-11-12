04638833b453e70bd7ee63464f0ab5a0f3d5d8fe
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-11-11 19:05:33 -0800

    refresh

--- app/src/main/java/io/husayn/paging_library_sample/listing/ExampleRemoteMediator.java
+++ app/src/main/java/io/husayn/paging_library_sample/listing/ExampleRemoteMediator.java
@@ -12,6 +12,7 @@ import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
+import timber.log.Timber;
 
 class ExampleRemoteMediator extends RxRemoteMediator<Integer, Pokemon> {
 
@@ -61,35 +62,53 @@ class ExampleRemoteMediator extends RxRemoteMediator<Integer, Pokemon> {
   @Override
   public Single<MediatorResult> loadSingle(
       @NonNull LoadType loadType, @NonNull PagingState<Integer, Pokemon> state) {
-    // The network load method takes an optional after=<Pokemon.id> parameter. For
-    // every page after the first, pass the last Pokemon ID to let it continue from
-    // where it left off. For REFRESH, pass null to load the first page.
-    Integer loadKey = null;
     switch (loadType) {
       case REFRESH:
-        break;
-      case PREPEND:
-        // In this example, you never need to prepend, since REFRESH will always
-        // load the first page in the list. Immediately return, reporting end of
-        // pagination.
-        return Single.just(new MediatorResult.Success(true));
+        return refresh(loadType, state);
       case APPEND:
-        Pokemon lastItem = state.lastItemOrNull();
-
-        // You must explicitly check if the last item is null when appending,
-        // since passing null to networkService is only valid for initial load.
-        // If lastItem is null it means no items were loaded after the initial
-        // REFRESH and there are no more items to load.
-        if (lastItem == null) {
-          return Single.just(new MediatorResult.Success(true));
-        }
-
-        loadKey = lastItem.id;
-        break;
+        return append(loadType, state);
+      case PREPEND:
+      default:
+        return ignorePrepend();
     }
+  }
 
-    return networkService
-        .searchPokemons(query, loadKey)
+  /**
+   * In this example, you never need to prepend, since REFRESH will always load the first page in
+   * the list. Immediately return, reporting end of pagination.
+   */
+  private Single<MediatorResult> ignorePrepend() {
+    Timber.w("Prepend LoadType ignored");
+    return Single.just(new MediatorResult.Success(true));
+  }
+  /**
+   * The network load method takes an optional after=<Pokemon.id> parameter. For every page after
+   * the first, pass the last Pokemon ID to let it continue from where it left off. For REFRESH,
+   * pass null to load the first page.
+   */
+  private Single<MediatorResult> refresh(LoadType loadType, PagingState<Integer, Pokemon> state) {
+    Timber.w("refresh :%s", loadType);
+    return fetch(loadType, networkService.searchPokemons(query, null));
+  }
+
+  /**
+   * You must explicitly check if the last item is null when appending, since passing null to
+   * networkService is only valid for initial load. If lastItem is null it means no items were
+   * loaded after the initial ø REFRESH and there are no more items to load.
+   */
+  private Single<MediatorResult> append(LoadType loadType, PagingState<Integer, Pokemon> state) {
+    Timber.w("append :%s", loadType);
+    Pokemon lastItem = state.lastItemOrNull();
+    if (lastItem == null) {
+      return Single.just(new MediatorResult.Success(true));
+    } else {
+      return fetch(loadType, networkService.searchPokemons(query, lastItem.id));
+    }
+  }
+
+  private Single<MediatorResult> fetch(
+      LoadType loadType, Single<SearchPokemonResponse> searchPokemonResponseSingle) {
+    return searchPokemonResponseSingle
         .subscribeOn(Schedulers.io())
         .map(
             (Function<SearchPokemonResponse, MediatorResult>)
