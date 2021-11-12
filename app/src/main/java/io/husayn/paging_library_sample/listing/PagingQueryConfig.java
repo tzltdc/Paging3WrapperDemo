f71af429cb22c4c630f96c307f23a74305ee8c04
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-11-11 22:47:12 -0800

    MAX_CONFIG

--- app/src/main/java/io/husayn/paging_library_sample/listing/PagingQueryConfig.java
+++ app/src/main/java/io/husayn/paging_library_sample/listing/PagingQueryConfig.java
@@ -3,6 +3,7 @@ package io.husayn.paging_library_sample.listing;
 class PagingQueryConfig {
 
   public static final PagingQueryConfig DEFAULT_QUERY_CONFIG = new PagingQueryConfig(10);
+  public static final PagingQueryConfig MAX_CONFIG = new PagingQueryConfig(1000);
   public final int countPerPage;
 
   public PagingQueryConfig(int countPerPage) {
--- app/src/test/java/io/husayn/paging_library_sample/listing/ExampleBackendServiceTest.java
+++ app/src/test/java/io/husayn/paging_library_sample/listing/ExampleBackendServiceTest.java
@@ -2,6 +2,7 @@ package io.husayn.paging_library_sample.listing;
 
 import static com.google.common.truth.Truth.assertThat;
 
+import androidx.annotation.Nullable;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
@@ -11,11 +12,29 @@ public class ExampleBackendServiceTest {
 
   @Test
   public void initial_queryWithoutFilter_shouldReturn10() {
-    assertThat(ExampleBackendService.query(firstRequest()).test().values().get(0).getPokemons())
-        .hasSize(10);
+    assertThat(ExampleBackendService.query(firstRequest(null)).test().values().get(0).getPokemons())
+        .hasSize(151);
   }
 
-  private PagingRequest firstRequest() {
-    return new PagingRequest(0, new PagingQuery(null), PagingQueryConfig.DEFAULT_QUERY_CONFIG);
+  @Test
+  public void initial_queryWithFilter_shouldReturn10() {
+    assertThat(ExampleBackendService.query(firstRequest("a")).test().values().get(0).getPokemons())
+        .hasSize(83);
+  }
+
+  @Test
+  public void initial_queryWithDoubleFilter_shouldReturn10() {
+    assertThat(ExampleBackendService.query(firstRequest("aa")).test().values().get(0).getPokemons())
+        .hasSize(0);
+  }
+
+  @Test
+  public void initial_queryWithAb_shouldReturn10() {
+    assertThat(ExampleBackendService.query(firstRequest("ab")).test().values().get(0).getPokemons())
+        .hasSize(6);
+  }
+
+  private PagingRequest firstRequest(@Nullable String searchKey) {
+    return new PagingRequest(0, new PagingQuery(searchKey), PagingQueryConfig.MAX_CONFIG);
   }
 }
