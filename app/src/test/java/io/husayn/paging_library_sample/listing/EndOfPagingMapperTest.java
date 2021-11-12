f3da49345f7360bcb3f8354286698f2c7ddeeeea
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-11-11 22:55:22 -0800

    @RunWith(RobolectricTestRunner.class)

---
+++ app/src/test/java/io/husayn/paging_library_sample/listing/EndOfPagingMapperTest.java
@@ -0,0 +1,14 @@
+package io.husayn.paging_library_sample.listing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class EndOfPagingMapperTest {
+
+  @Test
+  public void endOfPaging() {}
+}
