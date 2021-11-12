13f6b299e9b542803d458a434ee27938e62952d8
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-11-11 19:27:40 -0800

    Add server

---
+++ app/src/main/java/io/husayn/paging_library_sample/data/RemoteDataServer.java
@@ -0,0 +1,25 @@
+package io.husayn.paging_library_sample.data;
+
+import io.husayn.paging_library_sample.PokemonApplication;
+import io.husayn.paging_library_sample.R;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class RemoteDataServer {
+
+  private static List<Pokemon> pokemonList() {
+    return Collections.unmodifiableList(insertInternal());
+  }
+
+  private static List<Pokemon> insertInternal() {
+    List<Pokemon> list = new ArrayList<>();
+    String[] name =
+        PokemonApplication.getContext().getResources().getStringArray(R.array.pokemon_names);
+    int index = 0;
+    for (String pokemonName : name) {
+      list.add(new Pokemon(++index, pokemonName));
+    }
+    return list;
+  }
+}
