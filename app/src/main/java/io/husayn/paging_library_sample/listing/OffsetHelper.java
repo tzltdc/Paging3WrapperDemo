ec3dd901390c5df1099a264d8db2a0708842f6fd
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-11-11 23:25:31 -0800

    tag("")

--- app/src/main/java/io/husayn/paging_library_sample/listing/OffsetHelper.java
+++ app/src/main/java/io/husayn/paging_library_sample/listing/OffsetHelper.java
@@ -3,11 +3,18 @@ package io.husayn.paging_library_sample.listing;
 import io.husayn.paging_library_sample.data.Pokemon;
 import io.husayn.paging_library_sample.data.RemoteDataServer;
 import io.reactivex.Observable;
+import timber.log.Timber;
 
 class OffsetHelper {
 
   /** Temporarily using the remote service to identify the offset position */
   public static long offset(Pokemon lastFetchedAsTarget, PagingQuery query) {
+    long loaded = offSet(lastFetchedAsTarget, query);
+    Timber.i("loaded:%s, query:%s", loaded, query.searchKey());
+    return loaded;
+  }
+
+  private static Long offSet(Pokemon lastFetchedAsTarget, PagingQuery query) {
     return Observable.fromIterable(RemoteDataServer.all())
         .filter(item -> ExampleBackendService.validItem(lastFetchedAsTarget, query))
         .takeUntil(
