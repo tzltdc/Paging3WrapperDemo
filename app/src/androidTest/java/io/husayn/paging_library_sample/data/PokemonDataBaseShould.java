33a8fa713938fb6d1f4352476feeff7c97fa911a
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-11-12 23:03:53 -0800

    Add TestAppComponent to test PokemonDataBaseShould

--- app/src/androidTest/java/io/husayn/paging_library_sample/data/PokemonDataBaseShould.java
+++ app/src/androidTest/java/io/husayn/paging_library_sample/data/PokemonDataBaseShould.java
@@ -2,24 +2,34 @@ package io.husayn.paging_library_sample.data;
 
 import static junit.framework.Assert.assertEquals;
 
+import android.app.Application;
 import android.content.Context;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
+import javax.inject.Inject;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /** Created by husaynhakeem on 9/26/17. */
+@RunWith(AndroidJUnit4.class)
 public class PokemonDataBaseShould {
 
   public static final String ANY_POKEMON_NAME = "pokemon";
-  private PokemonDao dao;
+  @Inject PokemonDao dao;
 
   @Before
   public void setUp() {
-
-    Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
-    dao = PokemonDataBase.getInstance(context).pokemonDao();
+    Application application = testApplication();
+    DaggerTestAppComponent.factory().newMyComponent(application).inject(this);
     dao.deleteAll();
+  }
+
+  public static Application testApplication() {
+    Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+    Context applicationContext = context.getApplicationContext();
+    return (Application) applicationContext;
   }
 
   @Test
@@ -37,12 +47,16 @@ public class PokemonDataBaseShould {
 
   @After
   public void tearDown() {
-    if (dao != null) dao.deleteAll();
+    if (dao != null) {
+      dao.deleteAll();
+    }
   }
 
   private Pokemon[] anyPokemons() {
     Pokemon[] pokemons = new Pokemon[10];
-    for (int i = 0; i < 10; i++) pokemons[i] = new Pokemon(i, ANY_POKEMON_NAME);
+    for (int i = 0; i < 10; i++) {
+      pokemons[i] = new Pokemon(i, ANY_POKEMON_NAME);
+    }
     return pokemons;
   }
 }
