a36e61d4aaefb7e1e19528431e5c2f79827e5652
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-11-12 20:23:33 -0800

    Add empty AppComponent

---
+++ app/src/main/java/io/husayn/paging_library_sample/AppScope.java
@@ -0,0 +1,17 @@
+package io.husayn.paging_library_sample;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import javax.inject.Scope;
+
+/**
+ * Identifies a type that the injector only instantiates once. Not inherited.
+ *
+ * @see javax.inject.Scope @Scope
+ */
+@Scope
+@Documented
+@Retention(RUNTIME)
+public @interface AppScope {}
