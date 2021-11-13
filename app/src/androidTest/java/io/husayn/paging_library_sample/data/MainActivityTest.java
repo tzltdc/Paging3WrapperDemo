85eac82dafef9f1dd8959ffd89300febfddccbb0
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-11-13 09:44:44 -0800

    Add MainActivityTest with app loading state.

---
+++ app/src/androidTest/java/io/husayn/paging_library_sample/data/MainActivityTest.java
@@ -0,0 +1,33 @@
+package io.husayn.paging_library_sample.data;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+import io.husayn.paging_library_sample.listing.MainActivity;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityTest {
+
+  @Rule
+  public ActivityScenarioRule<MainActivity> rule = new ActivityScenarioRule<>(MainActivity.class);
+
+  @Test
+  public void onAppStarts_shouldAllData() {
+    ViewInteraction totalViewCount = onView(withText("total:151"));
+    totalViewCount.check(matches(isDisplayed()));
+
+    ViewInteraction absentViewCount = onView(withText("total:152"));
+    absentViewCount.check(doesNotExist());
+  }
+}
