1b0c2064fe0975f65b98d7cf1578c86f0b016d75
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-11-12 22:30:37 -0800

    Add database related dependencies to inject the worker.

--- app/src/main/java/io/husayn/paging_library_sample/ApplicationDerivedModule.java
+++ app/src/main/java/io/husayn/paging_library_sample/ApplicationDerivedModule.java
@@ -4,10 +4,12 @@ import android.app.Application;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import dagger.Module;
+import dagger.Provides;
 
 @Module
-public abstract class ApplicationDerivedModule {
+public class ApplicationDerivedModule {
 
+  @Provides
   @AppScope
   public SharedPreferences appSharedPreferences(Application application) {
     return PreferenceManager.getDefaultSharedPreferences(application);
