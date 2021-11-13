ed7fe83859f473ae865c63f11ec9f57371dfc5e1
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-11-12 20:27:29 -0800

    Add minimum dagger factory.

--- app/src/main/java/io/husayn/paging_library_sample/AppComponent.java
+++ app/src/main/java/io/husayn/paging_library_sample/AppComponent.java
@@ -1,7 +1,17 @@
 package io.husayn.paging_library_sample;
 
+import android.app.Application;
+import dagger.BindsInstance;
 import dagger.Component;
+import dagger.android.AndroidInjector;
 
 @AppScope
 @Component
-public interface AppComponent {}
+public interface AppComponent extends AndroidInjector<AppComponent> {
+
+  @Component.Factory
+  interface Factory {
+
+    AppComponent newMyComponent(@BindsInstance Application application);
+  }
+}
