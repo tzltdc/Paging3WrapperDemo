9eb73857bab55a2ffdfdf4a25232eb0c15cb5517
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-11-12 23:03:53 -0800

    Add TestAppComponent to test PokemonDataBaseShould

---
+++ app/src/androidTest/java/io/husayn/paging_library_sample/data/TestAppComponent.java
@@ -0,0 +1,20 @@
+package io.husayn.paging_library_sample.data;
+
+import android.app.Application;
+import dagger.BindsInstance;
+import dagger.Component;
+import io.husayn.paging_library_sample.AppModule;
+import io.husayn.paging_library_sample.AppScope;
+
+@AppScope
+@Component(modules = {AppModule.class})
+public interface TestAppComponent {
+
+  void inject(PokemonDataBaseShould pokemonDataBaseShould);
+
+  @Component.Factory
+  interface Factory {
+
+    TestAppComponent newMyComponent(@BindsInstance Application application);
+  }
+}
