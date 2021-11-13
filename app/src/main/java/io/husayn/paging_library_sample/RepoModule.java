9446148b82a353c66874afd4c10265bddbd5206c
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-11-12 22:30:37 -0800

    Add database related dependencies to inject the worker.

--- app/src/main/java/io/husayn/paging_library_sample/RepoModule.java
+++ app/src/main/java/io/husayn/paging_library_sample/RepoModule.java
@@ -2,5 +2,5 @@ package io.husayn.paging_library_sample;
 
 import dagger.Module;
 
-@Module(includes = DatabaseModule.class)
+@Module(includes = {DatabaseModule.class})
 public abstract class RepoModule {}
