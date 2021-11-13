800d71f6a7d7ae6d1f85d71f62dadf9130b8ecc3
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-11-13 11:40:58 -0800

    Fix timber log.

--- app/src/main/java/io/husayn/paging_library_sample/listing/MainActivity.java
+++ app/src/main/java/io/husayn/paging_library_sample/listing/MainActivity.java
@@ -86,7 +86,7 @@ public class MainActivity extends AppCompatActivity
 
   @Override
   public void onItemClick(Pokemon pokemon) {
-    Timber.i("pokemon:%s", pokemon);
+    Timber.i("onItemClick:%s", pokemon);
     startActivity(DetailActivity.construct(PokemonId.create(pokemon.id), this));
   }
 
