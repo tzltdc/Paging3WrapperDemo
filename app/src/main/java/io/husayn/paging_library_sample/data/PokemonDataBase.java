85d38d2de6b17536cee7842f7ae8d86a95148121
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-11-12 23:04:44 -0800

    Clean up PokemonDataBase

--- app/src/main/java/io/husayn/paging_library_sample/data/PokemonDataBase.java
+++ app/src/main/java/io/husayn/paging_library_sample/data/PokemonDataBase.java
@@ -1,27 +1,12 @@
 package io.husayn.paging_library_sample.data;
 
-import android.content.Context;
 import androidx.room.Database;
-import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 @Database(
     entities = {Pokemon.class, Repo.class, User.class, UserRepoJoin.class},
     version = 1)
 public abstract class PokemonDataBase extends RoomDatabase {
-
-  private static final String POKEMON_DB = "pokemon.db";
-  private static PokemonDataBase pokemonDB;
-
-  public static PokemonDataBase getInstance(Context context) {
-    if (pokemonDB == null) {
-      pokemonDB =
-          Room.databaseBuilder(context.getApplicationContext(), PokemonDataBase.class, POKEMON_DB)
-              .allowMainThreadQueries()
-              .build();
-    }
-    return pokemonDB;
-  }
 
   public abstract PokemonDao pokemonDao();
 
