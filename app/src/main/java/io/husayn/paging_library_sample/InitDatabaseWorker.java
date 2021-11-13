b7e9763528170f197d2bfa360d7c6fc0440da8b2
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-11-12 23:16:39 -0800

    Add AppConfig logic into InitDatabaseWorker

--- app/src/main/java/io/husayn/paging_library_sample/InitDatabaseWorker.java
+++ app/src/main/java/io/husayn/paging_library_sample/InitDatabaseWorker.java
@@ -1,21 +1,31 @@
 package io.husayn.paging_library_sample;
 
 import com.uber.autodispose.ScopeProvider;
+import io.app.config.AppConfig;
 import javax.inject.Inject;
 
 public class InitDatabaseWorker implements AutoDisposeWorker {
 
+  private final AppConfig appConfig;
   private final AppStatusRepo appStatusRepo;
   private final PokemonRepo pokemonRepo;
 
   @Inject
-  public InitDatabaseWorker(AppStatusRepo appStatusRepo, PokemonRepo pokemonRepo) {
+  public InitDatabaseWorker(
+      AppConfig appConfig, AppStatusRepo appStatusRepo, PokemonRepo pokemonRepo) {
+    this.appConfig = appConfig;
     this.appStatusRepo = appStatusRepo;
     this.pokemonRepo = pokemonRepo;
   }
 
   @Override
   public void attach(ScopeProvider scopeProvider) {
+    if (appConfig.initializeDatabase()) {
+      initIfNeeded();
+    }
+  }
+
+  private void initIfNeeded() {
     if (!appStatusRepo.initialized()) {
       pokemonRepo.populateDB();
       appStatusRepo.markAsInitialized();
