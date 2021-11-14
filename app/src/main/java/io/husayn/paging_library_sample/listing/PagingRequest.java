d53745823d9ca9573d8c9f688fec59f12045476e
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-11-13 22:30:38 -0800

    Remove loadType in PagingRequest

--- app/src/main/java/io/husayn/paging_library_sample/listing/PagingRequest.java
+++ app/src/main/java/io/husayn/paging_library_sample/listing/PagingRequest.java
@@ -1,12 +1,9 @@
 package io.husayn.paging_library_sample.listing;
 
 import com.google.auto.value.AutoValue;
-import io.husayn.paging_library_sample.listing.PagingQueryAction.LoadType;
 
 @AutoValue
 abstract class PagingRequest {
-
-  public abstract LoadType type();
 
   public abstract long offSet();
 
@@ -15,10 +12,7 @@ abstract class PagingRequest {
   public abstract PagingRemoteRequestConfig queryConfig();
 
   public static PagingRequest create(
-      long offSet,
-      LoadType loadType,
-      PagingQueryParam pagingQueryParam,
-      PagingRemoteRequestConfig queryConfig) {
-    return new AutoValue_PagingRequest(loadType, offSet, pagingQueryParam, queryConfig);
+      long offSet, PagingQueryParam pagingQueryParam, PagingRemoteRequestConfig queryConfig) {
+    return new AutoValue_PagingRequest(offSet, pagingQueryParam, queryConfig);
   }
 }
