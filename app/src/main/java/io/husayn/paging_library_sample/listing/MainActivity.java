766df02ee6e341414aceb9bf07aaf1ae4fd54519
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-11-13 20:33:05 -0800

    Use ConcatAdapter to support header and footer.

--- app/src/main/java/io/husayn/paging_library_sample/listing/MainActivity.java
+++ app/src/main/java/io/husayn/paging_library_sample/listing/MainActivity.java
@@ -3,12 +3,16 @@ package io.husayn.paging_library_sample.listing;
 import static com.uber.autodispose.AutoDispose.autoDisposable;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.paging.CombinedLoadStates;
 import androidx.paging.PagingConfig;
 import androidx.paging.PagingData;
+import androidx.recyclerview.widget.ConcatAdapter;
+import androidx.recyclerview.widget.ConcatAdapter.Config;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.GridLayoutManager.SpanSizeLookup;
 import androidx.recyclerview.widget.RecyclerView;
 import com.uber.autodispose.android.lifecycle.AndroidLifecycleScopeProvider;
 import dagger.Binds;
@@ -19,6 +23,12 @@ import io.husayn.paging_library_sample.R;
 import io.husayn.paging_library_sample.data.Pokemon;
 import io.husayn.paging_library_sample.listing.PokemonViewHolder.OnItemClickCallback;
 import io.husayn.paging_library_sample.listing.QueryViewHolder.QueryCallback;
+import io.thread.MainScheduler;
+import io.view.header.FooterAdapter;
+import io.view.header.FooterEntity;
+import io.view.header.HeaderAdapter;
+import io.view.header.HeaderEntity;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 import javax.inject.Inject;
@@ -30,8 +40,11 @@ public class MainActivity extends AppCompatActivity
 
   @Inject QueryStream queryStream;
   @Inject PagingPokemonRepo pagingPokemonRepo;
-  @Inject PokemonAdapter adapter;
+  @Inject PokemonAdapter pokemonAdapter;
+  @Inject HeaderAdapter headerAdapter;
   @Inject QueryAdapter queryAdapter;
+  @Inject FooterAdapter footerAdapter;
+  private ConcatAdapter concatAdapter;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -44,17 +57,36 @@ public class MainActivity extends AppCompatActivity
   }
 
   private void bindRecyclerView() {
-    adapter.addLoadStateListener(this::onStateChanged);
+    pokemonAdapter.addLoadStateListener(this::onStateChanged);
     RecyclerView recyclerView = findViewById(R.id.rv_pokemons);
     recyclerView.setHasFixedSize(true);
-    recyclerView.setLayoutManager(
-        new GridLayoutManager(this, getResources().getInteger(R.integer.span_count)));
-    recyclerView.setAdapter(adapter);
+    recyclerView.setLayoutManager(layout());
+    concatAdapter =
+        new ConcatAdapter(config(), Arrays.asList(headerAdapter, pokemonAdapter, footerAdapter));
+    recyclerView.setAdapter(concatAdapter);
+  }
+
+  private Config config() {
+    return new Config.Builder().setIsolateViewTypes(false).build();
+  }
+
+  private GridLayoutManager layout() {
+    GridLayoutManager manager = new GridLayoutManager(this, ItemViewType.SPAN_FULL);
+    MySpanSizeLookup spanSizeLookup = new MySpanSizeLookup();
+    spanSizeLookup.setSpanIndexCacheEnabled(true);
+    manager.setSpanSizeLookup(spanSizeLookup);
+    return manager;
+  }
+
+  /** */
+  private void retry(View view) {
+    pokemonAdapter.retry();
   }
 
   private void bindPagingData() {
     pagingPokemonRepo
         .rxPagingData()
+        .observeOn(MainScheduler.get())
         .as(autoDisposable(AndroidLifecycleScopeProvider.from(this)))
         .subscribe(this::submitList);
   }
@@ -66,22 +98,33 @@ public class MainActivity extends AppCompatActivity
     queryRecyclerView.setAdapter(queryAdapter);
   }
 
-  private void submitList(PagingData<Pokemon> pokemonPagingData) {
-    adapter.submitData(getLifecycle(), pokemonPagingData);
+  private void submitList(PagingData<Pokemon> pagingData) {
+    Timber.i("onStateChanged submitList:%s", pagingData);
+    pokemonAdapter.submitData(getLifecycle(), pagingData);
   }
 
   private Unit onStateChanged(CombinedLoadStates state) {
-    Timber.i("onStateChanged:%s", state);
-    findViewById(R.id.tv_count).post(this::updateStatus);
+    List<Pokemon> snapshot = pokemonAdapter.snapshot().getItems();
+    String summary = content(snapshot);
+    HeaderEntity headerEntity =
+        StateMapper.headerEntity(PagingViewModel.create(state.getRefresh(), snapshot));
+
+    FooterEntity footerEntity =
+        StateMapper.footerEntity(PagingViewModel.create(state.getAppend(), snapshot));
+    Timber.i(
+        "onStateChanged header:%s,footer:%s,snapshot:%s", headerEntity, footerEntity, snapshot);
+    headerAdapter.bind(headerEntity);
+    footerAdapter.bind(footerEntity);
+    findViewById(R.id.tv_count).post(() -> updateStatus(summary));
     return Unit.INSTANCE;
   }
 
-  private void updateStatus() {
-    ((TextView) findViewById(R.id.tv_count)).setText(content());
+  private void updateStatus(String content) {
+    ((TextView) findViewById(R.id.tv_count)).setText(content);
   }
 
-  private String content() {
-    return String.format(Locale.getDefault(), "total:%d", adapter.getItemCount());
+  private String content(List<Pokemon> snapshot) {
+    return String.format(Locale.getDefault(), "total:%d", snapshot.size());
   }
 
   @Override
@@ -98,8 +141,8 @@ public class MainActivity extends AppCompatActivity
   @dagger.Module
   public abstract static class Module {
 
-    private static final int PAGE_SIZE = 20;
-    private static final int PREFETCH_DISTANCE = 5;
+    private static final int PAGE_SIZE = 2;
+    private static final int PREFETCH_DISTANCE = 2;
 
     @ActivityScope
     @Provides
@@ -135,5 +178,14 @@ public class MainActivity extends AppCompatActivity
     @Binds
     public abstract PokemonViewHolder.OnItemClickCallback pokemonViewHolderOnItemClickCallback(
         MainActivity mainActivity);
+  }
+
+  private class MySpanSizeLookup extends SpanSizeLookup {
+
+    @Override
+    public int getSpanSize(int position) {
+      int itemViewType = concatAdapter.getItemViewType(position);
+      return ItemViewType.map(itemViewType);
+    }
   }
 }
