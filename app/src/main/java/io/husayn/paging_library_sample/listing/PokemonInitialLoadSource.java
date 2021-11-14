6b7181699dc40d8f5a92732c833827767ce9a5ee
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-11-13 21:23:35 -0800

    Add PokemonInitialLoadSource.java

---
+++ app/src/main/java/io/husayn/paging_library_sample/listing/PokemonInitialLoadSource.java
@@ -0,0 +1,74 @@
+package io.husayn.paging_library_sample.listing;
+
+import androidx.paging.LoadType;
+import androidx.paging.RemoteMediator.MediatorResult;
+import androidx.paging.RemoteMediator.MediatorResult.Success;
+import io.husayn.paging_library_sample.listing.PagingAction.Data;
+import io.reactivex.Single;
+import io.thread.WorkerScheduler;
+import java.util.concurrent.TimeUnit;
+import javax.inject.Inject;
+import timber.log.Timber;
+
+public class PokemonInitialLoadSource {
+
+  private final PokemonRepo pokemonRepo;
+  private final PokemonRemoteSource pokemonRemoteSource;
+  private final WorkerScheduler scheduler;
+
+  @Inject
+  public PokemonInitialLoadSource(
+      PokemonRemoteSource pokemonRemoteSource, PokemonRepo pokemonRepo, WorkerScheduler scheduler) {
+    this.pokemonRemoteSource = pokemonRemoteSource;
+    this.pokemonRepo = pokemonRepo;
+    this.scheduler = scheduler;
+  }
+
+  /**
+   * The network load method takes an optional after=<Pokemon.id> parameter. For every page after
+   * the first, pass the last Pokemon ID to let it continue from where it left off. For REFRESH,
+   * pass null to load the first page.
+   */
+  public Single<MediatorResult> load(PagingQuery query) {
+    Timber.i("initial load with query :%s", query);
+    return simulateError(query)
+        ? simulateError()
+        : execute(PagingRequestMapper.defaultPagingRequest(query));
+  }
+
+  private boolean simulateError(PagingQuery query) {
+    return FilterOptionProvider.INITIAL_LOAD_ERROR.equals(query.searchKey());
+  }
+
+  private Single<MediatorResult> simulateError() {
+    scheduler = null;
+    return Single.timer(1000, TimeUnit.MILLISECONDS, scheduler.get())
+        .map(ignored -> new MediatorResult.Error(new RuntimeException("Simulated error")));
+  }
+
+  private Single<MediatorResult> execute(PagingRequest pagingRequest) {
+    return pokemonRemoteSource
+        .fetch(pagingRequest)
+        .map(this::pagingAction)
+        .map(this::success)
+        .onErrorResumeNext(this::error);
+  }
+
+  private PagingAction pagingAction(Data data) {
+    return PagingAction.create(LoadType.REFRESH, data);
+  }
+
+  private Single<MediatorResult> error(Throwable e) {
+    Timber.e(e, "initial load error");
+    return Single.just(new MediatorResult.Error(e));
+  }
+
+  private MediatorResult success(PagingAction action) {
+    pokemonRepo.flushDbData(action);
+    return new Success(endOfPaging(action));
+  }
+
+  private boolean endOfPaging(PagingAction action) {
+    return EndOfPagingMapper.endOfPaging(action.data());
+  }
+}
