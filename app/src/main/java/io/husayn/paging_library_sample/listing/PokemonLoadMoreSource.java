0ce419917399f2dcd3f9418fb52322683d489c8a
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-11-13 21:29:51 -0800

    Add PokemonLoadMoreSource.java

---
+++ app/src/main/java/io/husayn/paging_library_sample/listing/PokemonLoadMoreSource.java
@@ -0,0 +1,85 @@
+package io.husayn.paging_library_sample.listing;
+
+import androidx.paging.LoadType;
+import androidx.paging.RemoteMediator.MediatorResult;
+import androidx.paging.RemoteMediator.MediatorResult.Success;
+import io.husayn.paging_library_sample.data.Pokemon;
+import io.husayn.paging_library_sample.listing.PagingAction.Data;
+import io.reactivex.Single;
+import io.thread.WorkerScheduler;
+import javax.inject.Inject;
+import timber.log.Timber;
+
+public class PokemonLoadMoreSource {
+
+  private final WorkerScheduler workerScheduler;
+  private final PokemonRepo pokemonRepo;
+  private final PokemonRemoteSource pokemonRemoteSource;
+
+  @Inject
+  public PokemonLoadMoreSource(
+      WorkerScheduler workerScheduler,
+      PokemonRepo pokemonRepo,
+      PokemonRemoteSource pokemonRemoteSource) {
+    this.workerScheduler = workerScheduler;
+    this.pokemonRepo = pokemonRepo;
+    this.pokemonRemoteSource = pokemonRemoteSource;
+  }
+
+  /**
+   * You must explicitly check if the last item is null when appending, since passing null to
+   * networkService is only valid for initial load. If lastItem is null it means no items were
+   * loaded after the initial ø REFRESH and there are no more items to load.
+   */
+  public Single<MediatorResult> loadingMore(PagingQuery query) {
+    return Single.just(query).flatMap(this::deferAppend).subscribeOn(workerScheduler.get());
+  }
+
+  private Single<MediatorResult> deferAppend(PagingQuery query) {
+    return showLoadMoreError(query) ? simulateError() : execute(query);
+  }
+
+  private boolean showLoadMoreError(PagingQuery query) {
+    return FilterOptionProvider.LOAD_MORE_ERROR.equals(query.searchKey());
+  }
+
+  private Single<MediatorResult> simulateError() {
+    return Single.just(new MediatorResult.Error(new RuntimeException("loading more happened")));
+  }
+
+  private Single<MediatorResult> execute(PagingQuery query) {
+    Pokemon lastItem = pokemonRepo.lastItemOrNull(query);
+    Timber.i("tonny append with query:%s, last_item:%s", query, lastItem);
+    if (lastItem == null) {
+      return Single.just(new Success(true));
+    } else {
+      return execute(PagingRequestMapper.nextPagingRequest(query, lastItem));
+    }
+  }
+
+  private Single<MediatorResult> execute(PagingRequest pagingRequest) {
+    return pokemonRemoteSource
+        .fetch(pagingRequest)
+        .map(this::pagingAction)
+        .map(this::success)
+        .onErrorResumeNext(this::error);
+  }
+
+  private PagingAction pagingAction(Data data) {
+    return PagingAction.create(LoadType.APPEND, data);
+  }
+
+  private Single<MediatorResult> error(Throwable e) {
+    Timber.e(e, "tonny error");
+    return Single.just(new MediatorResult.Error(e));
+  }
+
+  private MediatorResult success(PagingAction action) {
+    pokemonRepo.flushDbData(action);
+    return new Success(endOfPaging(action));
+  }
+
+  private boolean endOfPaging(PagingAction action) {
+    return EndOfPagingMapper.endOfPaging(action.data());
+  }
+}
