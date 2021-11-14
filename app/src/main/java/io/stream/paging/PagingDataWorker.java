09f83249b1da409d72638245601f52d5c99a8749
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-11-14 15:57:15 -0800

    Integrate PagingDataModule

--- app/src/main/java/io/stream/paging/PagingDataWorker.java
+++ app/src/main/java/io/stream/paging/PagingDataWorker.java
@@ -1,14 +1,30 @@
 package io.stream.paging;
 
+import static com.uber.autodispose.AutoDispose.autoDisposable;
+
 import com.uber.autodispose.ScopeProvider;
 import io.husayn.paging_library_sample.AutoDisposeWorker;
+import io.husayn.paging_library_sample.listing.PagingPokemonRepo;
+import io.thread.MainScheduler;
 import javax.inject.Inject;
 
 public class PagingDataWorker implements AutoDisposeWorker {
 
+  private final PagingPokemonRepo pagingPokemonRepo;
+  private final PagingDataStream pagingDataStream;
+
   @Inject
-  public PagingDataWorker() {}
+  public PagingDataWorker(PagingPokemonRepo pagingPokemonRepo, PagingDataStream pagingDataStream) {
+    this.pagingPokemonRepo = pagingPokemonRepo;
+    this.pagingDataStream = pagingDataStream;
+  }
 
   @Override
-  public void attach(ScopeProvider scopeProvider) {}
+  public void attach(ScopeProvider scopeProvider) {
+    pagingPokemonRepo
+        .rxPagingData()
+        .observeOn(MainScheduler.get())
+        .as(autoDisposable(scopeProvider))
+        .subscribe(pagingDataStream::accept);
+  }
 }
