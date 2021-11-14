d0d9a32e978c75a9e5a684d08eb2f74608a82fe4
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-11-13 22:19:02 -0800

    Move LoadType to PagingRequest

--- app/src/main/java/io/husayn/paging_library_sample/listing/PagingRequestMapper.java
+++ app/src/main/java/io/husayn/paging_library_sample/listing/PagingRequestMapper.java
@@ -1,17 +1,20 @@
 package io.husayn.paging_library_sample.listing;
 
 import io.husayn.paging_library_sample.data.Pokemon;
+import io.husayn.paging_library_sample.listing.PagingQueryAction.LoadType;
 
 public class PagingRequestMapper {
 
   public static PagingRequest nextPagingRequest(PagingQueryParam query, Pokemon lastItem) {
     return PagingRequest.create(
         OffsetHelper.offset(lastItem, query),
+        LoadType.REFRESH,
         query,
         PagingRemoteRequestConfig.DEFAULT_QUERY_CONFIG);
   }
 
   public static PagingRequest defaultPagingRequest(PagingQueryParam query) {
-    return PagingRequest.create(0, query, PagingRemoteRequestConfig.DEFAULT_QUERY_CONFIG);
+    return PagingRequest.create(
+        0, LoadType.APPEND, query, PagingRemoteRequestConfig.DEFAULT_QUERY_CONFIG);
   }
 }
