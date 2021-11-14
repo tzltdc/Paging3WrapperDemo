bc1cee03a537ce395a8264f6d7d31c497fa4a176
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-11-14 10:39:52 -0800

    Update PokemonAdapter

--- app/src/main/java/io/husayn/paging_library_sample/listing/PokemonAdapter.java
+++ app/src/main/java/io/husayn/paging_library_sample/listing/PokemonAdapter.java
@@ -7,11 +7,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.paging.PagingDataAdapter;
+import androidx.recyclerview.widget.RecyclerView.ViewHolder;
 import io.husayn.paging_library_sample.R;
 import io.husayn.paging_library_sample.data.Pokemon;
 import javax.inject.Inject;
 
-public class PokemonAdapter extends PagingDataAdapter<Pokemon, PokemonViewHolder> {
+public class PokemonAdapter extends PagingDataAdapter<Pokemon, ViewHolder> {
 
   private final PokemonViewHolderFactory pokemonViewHolderFactory;
 
@@ -28,18 +29,21 @@ public class PokemonAdapter extends PagingDataAdapter<Pokemon, PokemonViewHolder
 
   @NonNull
   @Override
-  public PokemonViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+  public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
     View itemView = LayoutInflater.from(getContext()).inflate(R.layout.item_pokemon, parent, false);
     return pokemonViewHolderFactory.create(itemView);
   }
 
   @Override
-  public void onBindViewHolder(@NonNull PokemonViewHolder holder, int position) {
-    Pokemon pokemon = getItem(position);
-    if (pokemon != null) {
-      holder.bindTo(pokemon);
-    } else {
-      holder.clear();
+  public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    if (holder instanceof PokemonViewHolder) {
+      PokemonViewHolder pokemonViewHolder = (PokemonViewHolder) holder;
+      Pokemon pokemon = getItem(position);
+      if (pokemon != null) {
+        pokemonViewHolder.bindTo(pokemon);
+      } else {
+        pokemonViewHolder.clear();
+      }
     }
   }
 }
