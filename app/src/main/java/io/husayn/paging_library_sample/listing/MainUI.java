502be56ad48dd4cb566ebc888caae8be5569cde7
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-11-14 21:50:56 -0800

    Remove PokemonAdapterManager

--- app/src/main/java/io/husayn/paging_library_sample/listing/MainUI.java
+++ app/src/main/java/io/husayn/paging_library_sample/listing/MainUI.java
@@ -1,8 +1,3 @@
 package io.husayn.paging_library_sample.listing;
 
-import io.stream.footer_entity.FooterModel;
-
-public interface MainUI {
-
-  void bindFooterModel(FooterModel model);
-}
+public interface MainUI {}
