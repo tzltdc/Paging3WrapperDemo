ad684fe2f6a15b931e3bcb6b36178f7a9fb9b620
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-11-14 21:54:47 -0800

    Simplify FooterModelWorker.java

--- app/src/main/java/io/stream/load_state/footer/FooterModelWorker.java
+++ app/src/main/java/io/stream/load_state/footer/FooterModelWorker.java
@@ -2,36 +2,29 @@ package io.stream.load_state.footer;
 
 import static com.uber.autodispose.AutoDispose.autoDisposable;
 
-import com.google.common.base.Optional;
 import com.uber.autodispose.ScopeProvider;
 import io.husayn.paging_library_sample.AutoDisposeWorker;
 import io.paging.footer.FooterEntityContract;
-import io.stream.footer_entity.FooterEntityStreaming;
 import io.stream.footer_entity.FooterModel;
-import io.view.header.FooterEntity;
 import javax.inject.Inject;
 import timber.log.Timber;
 
 public class FooterModelWorker implements AutoDisposeWorker {
 
-  private final FooterEntityStreaming footerEntityStreaming;
+  private final FooterModelStreaming footerModelStreaming;
   private final FooterEntityContract footerEntityContract;
 
   @Inject
   public FooterModelWorker(
-      FooterEntityStreaming footerEntityStreaming, FooterEntityContract footerEntityContract) {
-    this.footerEntityStreaming = footerEntityStreaming;
+      FooterModelStreaming footerModelStreaming, FooterEntityContract footerEntityContract) {
+    this.footerModelStreaming = footerModelStreaming;
     this.footerEntityContract = footerEntityContract;
   }
 
   @Override
   public void attach(ScopeProvider scopeProvider) {
-    footerEntityStreaming
-        .newFooterEntity()
-        .map(this::asFooterModel)
-        .filter(Optional::isPresent)
-        .map(Optional::get)
-        .distinctUntilChanged()
+    footerModelStreaming
+        .streaming()
         .as(autoDisposable(scopeProvider))
         .subscribe(this::bindFooterModel);
   }
@@ -48,20 +41,6 @@ public class FooterModelWorker implements AutoDisposeWorker {
       case TO_BE_REFRESHED:
         footerEntityContract.refreshFooter(model.toBeRefreshed());
         break;
-    }
-  }
-
-  private Optional<FooterModel> asFooterModel(Optional<FooterEntity> newFooterModel) {
-    FooterEntity newFooterEntity = newFooterModel.orNull();
-    FooterEntity previousFooter = footerEntityStreaming.currentFooterEntity().orNull();
-    if (newFooterEntity != null && previousFooter != null) {
-      return newFooterEntity.equals(previousFooter)
-          ? Optional.absent()
-          : Optional.of(FooterModel.ofToBeRefreshed(newFooterEntity));
-    } else if (newFooterEntity == null) {
-      return Optional.of(FooterModel.ofToBeRemoved());
-    } else {
-      return Optional.of(FooterModel.ofToBeAdded(newFooterEntity));
     }
   }
 }
