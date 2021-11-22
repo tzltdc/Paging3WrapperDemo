9797158eff0130dee5beee92b685d60010f2314b
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-11-21 17:10:04 -0800

    Add FeatureQueryModule.java

---
+++ app/src/main/java/paging/wrapper/demo/ui/query/FeatureQueryModule.java
@@ -0,0 +1,34 @@
+package paging.wrapper.demo.ui.query;
+
+import dagger.Binds;
+import dagger.Provides;
+import java.util.List;
+import paging.wrapper.demo.MainActivity;
+import paging.wrapper.di.app.ActivityScope;
+import paging.wrapper.model.data.FilterBean;
+
+@dagger.Module
+public abstract class FeatureQueryModule {
+
+  @ActivityScope
+  @Provides
+  public static List<FilterBean> query() {
+    return FilterOptionProvider.get();
+  }
+
+  @ActivityScope
+  @Provides
+  public static QueryStreamImpl queryStreamImpl() {
+    return new QueryStreamImpl();
+  }
+
+  @Binds
+  public abstract QueryStream bindQueryStream(QueryStreamImpl impl);
+
+  @Binds
+  public abstract QueryStreaming bindQueryStreaming(QueryStreamImpl impl);
+
+  @Binds
+  public abstract QueryViewHolder.QueryCallback queryViewHolderQueryCallback(
+      MainActivity mainActivity);
+}
