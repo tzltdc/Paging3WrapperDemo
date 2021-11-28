09869d789d545908102944f322487acfb5743a28
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-11-28 11:55:12 -0800

    Add minimum test for filter feature.

---
+++ app/src/androidTest/java/paging/wrapper/FilterFeatureTest.java
@@ -0,0 +1,44 @@
+package paging.wrapper;
+
+import static com.adevinta.android.barista.assertion.BaristaVisibilityAssertions.assertDisplayed;
+import static com.adevinta.android.barista.assertion.BaristaVisibilityAssertions.assertNotExist;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import paging.wrapper.demo.MainActivity;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class FilterFeatureTest {
+
+  @Rule
+  public ActivityScenarioRule<MainActivity> rule = new ActivityScenarioRule<>(MainActivity.class);
+
+  @Test
+  public void case_1_byDefaultAllFilterAreCaseSensitive() {
+    assertDisplayed("ALL");
+    assertNotExist("all");
+  }
+
+  @Test
+  public void case_2_filterWithSpaceWillNotBeFound() {
+    assertNotExist("ALL ");
+  }
+
+  @Test
+  public void case_3_byDefaultAllFilterOptionsAreDisplayed() {
+    assertDisplayed("ALL");
+    assertDisplayed("83 WITH A");
+    assertDisplayed("83 WITH A");
+    assertDisplayed("16 WITH B");
+    assertDisplayed("12 WITH EE");
+    assertDisplayed("1 IVY");
+    assertDisplayed("EMPTY");
+    assertDisplayed("INITIAL_LOAD_ERROR");
+    assertDisplayed("LOAD_MORE_ERROR");
+  }
+}
