005dcd665f9d3ca585b38c373691c065507c9b4e
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-11-28 15:55:36 -0800

    Disable IdlingRegistry idlingResource

--- app/src/androidTest/java/paging/wrapper/EmptyVIewStateTest.java
+++ app/src/androidTest/java/paging/wrapper/EmptyVIewStateTest.java
@@ -1,11 +1,13 @@
 package paging.wrapper;
 
 import static com.adevinta.android.barista.assertion.BaristaVisibilityAssertions.assertDisplayed;
-import static com.adevinta.android.barista.interaction.BaristaClickInteractions.clickOn;
+import static com.adevinta.android.barista.interaction.BaristaListInteractions.clickListItemChild;
+import static com.adevinta.android.barista.interaction.BaristaSleepInteractions.sleep;
 
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
+import io.husayn.paging_library_sample.R;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,8 +22,10 @@ public class EmptyVIewStateTest {
 
   @Test
   public void case_0_selectFilterColorShouldBeHighlighted() {
-    clickOn("EMPTY");
-    assertDisplayed("Initial Loading");
-    //    assertDisplayed("No data at all");
+    clickListItemChild(R.id.rv_query, 5, R.id.tv_query);
+    sleep(1500);
+    //    assertDisplayed("Initial Loading");
+    assertDisplayed("No data at all");
+    QueryStateAsserter.assertQueryTextColor(5, R.color.colorAccent);
   }
 }
--- app/src/main/java/paging/wrapper/test/AppIdleStateConsumerWorker.java
+++ app/src/main/java/paging/wrapper/test/AppIdleStateConsumerWorker.java
@@ -8,26 +8,35 @@ import javax.inject.Inject;
 import kotlin.Unit;
 import paging.wrapper.app.AutoDisposeWorker;
 import paging.wrapper.di.app.AppScope;
+import timber.log.Timber;
 
 @AppScope
 public class AppIdleStateConsumerWorker implements AutoDisposeWorker, IdlingResource {
 
+  private final AppIdleStateStream appIdleStateStream;
   private final AppIdleStateStreaming appIdleStateStreaming;
   private ResourceCallback resourceCallback;
 
   @Inject
-  public AppIdleStateConsumerWorker(AppIdleStateStreaming appIdleStateStreaming) {
+  public AppIdleStateConsumerWorker(
+      AppIdleStateStream appIdleStateStream, AppIdleStateStreaming appIdleStateStreaming) {
+    this.appIdleStateStream = appIdleStateStream;
     this.appIdleStateStreaming = appIdleStateStreaming;
+    Timber.i("IdlingResource created :%s", this);
   }
 
   @Override
   public void attach(ScopeProvider scopeProvider) {
+    Timber.i("IdlingResource attach:%s", this);
     appIdleStateStreaming.idling().as(autoDisposable(scopeProvider)).subscribe(this::accept);
   }
 
   private void accept(Unit unit) {
     if (resourceCallback != null) {
+      Timber.i("IdlingResource onTransitionToIdle:%s", this);
       resourceCallback.onTransitionToIdle();
+    } else {
+      Timber.w("IdlingResource skipped onTransitionToIdle:%s", this);
     }
   }
 
@@ -38,11 +47,15 @@ public class AppIdleStateConsumerWorker implements AutoDisposeWorker, IdlingReso
 
   @Override
   public boolean isIdleNow() {
-    return appIdleStateStreaming.peek();
+    boolean idle = appIdleStateStreaming.peek();
+    Timber.i("IdlingResource isIdleNow:%s", idle);
+    return idle;
   }
 
   @Override
   public void registerIdleTransitionCallback(ResourceCallback callback) {
+    appIdleStateStream.clear();
+    Timber.i("IdlingResource registerIdleTransitionCallback:%s", callback);
     this.resourceCallback = callback;
   }
 }
