3bb02fb98754396a1859d724bbdf4901076e4abb
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-11-28 13:00:34 -0800

    Prepare to test empty state.

---
+++ app/src/androidTest/java/paging/wrapper/EmptyVIewStateTest.java
@@ -0,0 +1,26 @@
+package paging.wrapper;
+
+import static com.adevinta.android.barista.assertion.BaristaVisibilityAssertions.assertDisplayed;
+import static com.adevinta.android.barista.interaction.BaristaClickInteractions.clickOn;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import paging.wrapper.demo.MainActivity;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class EmptyVIewStateTest {
+
+  @Rule
+  public ActivityScenarioRule<MainActivity> rule = new ActivityScenarioRule<>(MainActivity.class);
+
+  @Test
+  public void case_0_selectFilterColorShouldBeHighlighted() {
+    clickOn("EMPTY");
+    assertDisplayed("No data");
+  }
+}
