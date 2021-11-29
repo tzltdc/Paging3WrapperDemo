2495e47015d4f7cbcff05a578187b346cacc32c9
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-11-28 16:05:49 -0800

    Adjust the logic of registering the IdlingResource

--- app/src/androidTest/java/paging/wrapper/EspressoApp.java
+++ app/src/androidTest/java/paging/wrapper/EspressoApp.java
@@ -1,18 +1,11 @@
 package paging.wrapper;
 
-import android.util.Log;
-import androidx.test.espresso.IdlingResource;
 import paging.wrapper.app.PokemonApplication;
-import timber.log.Timber;
 
 public class EspressoApp extends PokemonApplication {
 
   @Override
   public void onCreate() {
     super.onCreate();
-    IdlingResource idlingResource = pagingIdlingResource();
-    Log.d("IdlingResource", String.format("EspressoApp IdlingResource:%s", idlingResource));
-    Timber.d("IdlingResource", String.format("EspressoApp IdlingResource:%s", idlingResource));
-    //    IdlingRegistry.getInstance().register(idlingResource);
   }
 }
