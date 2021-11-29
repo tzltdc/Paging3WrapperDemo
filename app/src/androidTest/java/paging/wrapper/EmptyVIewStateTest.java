e7324b480b83d793e5dc1b9a6a3ee806cd60c9b2
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-11-28 16:05:49 -0800

    Adjust the logic of registering the IdlingResource

--- app/src/androidTest/java/paging/wrapper/EmptyVIewStateTest.java
+++ app/src/androidTest/java/paging/wrapper/EmptyVIewStateTest.java
@@ -2,8 +2,9 @@ package paging.wrapper;
 
 import static com.adevinta.android.barista.assertion.BaristaVisibilityAssertions.assertDisplayed;
 import static com.adevinta.android.barista.interaction.BaristaListInteractions.clickListItemChild;
-import static com.adevinta.android.barista.interaction.BaristaSleepInteractions.sleep;
 
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.espresso.IdlingRegistry;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
@@ -23,8 +24,10 @@ public class EmptyVIewStateTest {
   @Test
   public void case_0_selectFilterColorShouldBeHighlighted() {
     clickListItemChild(R.id.rv_query, 5, R.id.tv_query);
-    sleep(1500);
-    //    assertDisplayed("Initial Loading");
+
+    EspressoApp espressoApp = (EspressoApp) ApplicationProvider.getApplicationContext();
+    IdlingRegistry.getInstance().register(espressoApp.pagingIdlingResource());
+
     assertDisplayed("No data at all");
     QueryStateAsserter.assertQueryTextColor(5, R.color.colorAccent);
   }
