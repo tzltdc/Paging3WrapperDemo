afd828f9f84cf34fa1ea6816a1b585b4c921642d
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-11-30 00:31:52 -0800

    Add StateMapperTest.java

---
+++ app/src/test/java/paging/wrapper/mapper/StateMapperTest.java
@@ -0,0 +1,47 @@
+package paging.wrapper.mapper;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import androidx.paging.LoadState.NotLoading;
+import java.util.Collections;
+import org.junit.Before;
+import org.junit.Test;
+import paging.wrapper.model.ui.HeaderEntity;
+import paging.wrapper.model.ui.HeaderEntity.Empty;
+import paging.wrapper.model.ui.PagingViewModel;
+
+public class StateMapperTest {
+
+  private StateMapper stateMapper;
+
+  @Before
+  public void setup() {
+    stateMapper = new StateMapper();
+  }
+
+  @Test
+  public void defaultLoadingUIConfig() {
+
+    assertThat(
+            stateMapper.footerEntity(PagingViewModel.create(notLoading(), Collections.emptyList())))
+        .isEqualTo(null);
+  }
+
+  private NotLoading notLoading() {
+    return new NotLoading(false);
+  }
+
+  @Test
+  public void headerEntity() {
+    assertThat(
+            stateMapper.headerEntity(PagingViewModel.create(notLoading(), Collections.emptyList())))
+        .isEqualTo(HeaderEntity.ofEmpty(Empty.create("No data at all")));
+  }
+
+  @Test
+  public void footerEntity() {
+    assertThat(
+            stateMapper.footerEntity(PagingViewModel.create(notLoading(), Collections.emptyList())))
+        .isEqualTo(null);
+  }
+}
