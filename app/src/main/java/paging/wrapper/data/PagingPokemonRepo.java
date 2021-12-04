185826bf38f542c9d4c0f4e108549c0d79983a2e
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-12-03 23:23:00 -0800

    Inline PagingPokemonRepo.java

--- app/src/main/java/paging/wrapper/data/PagingPokemonRepo.java
+++ app/src/main/java/paging/wrapper/data/PagingPokemonRepo.java
@@ -1,10 +1,56 @@
 package paging.wrapper.data;
 
+import androidx.paging.Pager;
 import androidx.paging.PagingData;
+import androidx.paging.rxjava2.PagingRx;
 import io.reactivex.Observable;
+import javax.inject.Inject;
+import paging.wrapper.demo.ui.query.QueryStreaming;
+import paging.wrapper.di.app.ActivityScope;
+import paging.wrapper.di.thread.AppScheduler;
+import paging.wrapper.model.data.PagingQueryContext;
 import paging.wrapper.model.data.Pokemon;
+import timber.log.Timber;
 
-public interface PagingPokemonRepo {
+@ActivityScope
+public class PagingPokemonRepo {
 
-  Observable<PagingData<Pokemon>> rxPagingData();
+  private final QueryStreaming queryStreaming;
+  private final AppScheduler appScheduler;
+  private final PagerFactory pagerFactory;
+
+  @Inject
+  public PagingPokemonRepo(
+      QueryStreaming queryStreaming, AppScheduler appScheduler, PagerFactory pagerFactory) {
+    this.queryStreaming = queryStreaming;
+    this.appScheduler = appScheduler;
+    this.pagerFactory = pagerFactory;
+  }
+
+  public Observable<PagingData<Pokemon>> rxPagingData() {
+    return queryStreaming
+        .streaming()
+        .doOnNext(this::logOnQueryEmitted)
+        .observeOn(appScheduler.worker())
+        .doOnNext(this::logOnThreadSwitched)
+        .map(this::pager)
+        .switchMap(PagingRx::getObservable)
+        .doOnNext(this::logOnRepoEmitted);
+  }
+
+  private void logOnRepoEmitted(PagingData<Pokemon> pokemonPagingData) {
+    Timber.i("[ttt]:Returned user query with PagingData :%s", pokemonPagingData);
+  }
+
+  private void logOnThreadSwitched(PagingQueryContext context) {
+    Timber.i("[ttt]:user query context emitted after switch thread:%s", context);
+  }
+
+  private void logOnQueryEmitted(PagingQueryContext context) {
+    Timber.i("[ttt]:user query context emitted prior switch thread:%s", context);
+  }
+
+  public Pager<Integer, Pokemon> pager(PagingQueryContext query) {
+    return pagerFactory.pager(query);
+  }
 }
--- app/src/main/java/paging/wrapper/di/PagingDataModule.java
+++ app/src/main/java/paging/wrapper/di/PagingDataModule.java
@@ -5,8 +5,6 @@ import dagger.Module;
 import dagger.Provides;
 import paging.wrapper.data.PagerFactory;
 import paging.wrapper.data.PagerFactoryImpl;
-import paging.wrapper.data.PagingPokemonRepo;
-import paging.wrapper.data.PagingPokemonRepoDbWithNetwork;
 import paging.wrapper.di.app.ActivityScope;
 import paging.wrapper.stream.PagingDataStream;
 import paging.wrapper.stream.PagingDataStreamImpl;
@@ -28,10 +26,6 @@ public abstract class PagingDataModule {
   @ActivityScope
   @Binds
   public abstract PagingDataStreaming footerLoadStateStreaming(PagingDataStreamImpl impl);
-
-  @ActivityScope
-  @Binds
-  public abstract PagingPokemonRepo pagingPokemonRepo(PagingPokemonRepoDbWithNetwork impl);
 
   @ActivityScope
   @Binds
